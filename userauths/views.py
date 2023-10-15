30de5620d24f15ec4fb36661c3ba2300c3a133a8
Md. Mountasir Rahaman <141022092+mounsiam@users.noreply.github.com>
2023-10-15 16:24:05 +0600

    Initial commit

---
+++ userauths/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.core.paginator import Paginator
+from django.db import transaction
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+
+
+from post.models import Post, Follow, Stream
+from django.contrib.auth.models import User
+from userauths.models import Profile
+from .forms import EditProfileForm, UserRegisterForm
+from django.urls import resolve
+from comment.models import Comment
+
+# Create your views here.
+def UserProfile(request, username):
+    Profile.objects.get_or_create(user=request.user)
+    user = get_object_or_404(User, username=username)
+    profile = Profile.objects.get(user=user)
+    url_name = resolve(request.path).url_name
+    posts = Post.objects.filter(user=user).order_by('-posted')
+
+    if url_name == 'profile':
+        posts = Post.objects.filter(user=user).order_by('-posted')
+    else:
+        posts = profile.favourite.all()
+
+    # Profile Stats
+    posts_count = Post.objects.filter(user=user).count()
+    following_count = Follow.objects.filter(follower=user).count()
+    followers_count = Follow.objects.filter(following=user).count()
+    # count_comment = Comment.objects.filter(post=posts).count()
+    follow_status = Follow.objects.filter(following=user, follower=request.user).exists()
